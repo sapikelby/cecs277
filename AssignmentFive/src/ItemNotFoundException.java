973bdd43a513004a54344a11b2d696cd28f91c04
Kelby Sapien <sapikelby@gmail.com>
2014-03-20 15:53:59 -0700

    added javadoc

--- AssignmentFive/src/ItemNotFoundException.java
+++ AssignmentFive/src/ItemNotFoundException.java
@@ -1,6 +1,6 @@
 /**
 *
-* CECS 277 Spring 2014.   Assignment 3
+* CECS 277 Spring 2014.   Assignment 5
 *
 * @author Kelby Sapien 3260
 * @author Paul Kim 6816
@@ -9,14 +9,13 @@
 import java.lang.Exception;
 
 /**
- * Exceptions class for overdrawing exceptions
+ * Exceptions class for ItemNotFoundException exceptions
  * Extends the Exception's class
  */
 public class ItemNotFoundException extends Exception
 {	
 	/**
-	 * constructor, calls superclass's constructor 
-	 * increments the count by 1
+	 * constructor, calls superclass's constructor
 	 * @param message message printed when exception is thrown
 	 */
 	public ItemNotFoundException(String message)
