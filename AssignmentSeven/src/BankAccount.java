2c0fef91bd8a2b645dca118394d8e30db36349c7
Kelby Sapien <sapikelby@gmail.com>
2014-04-15 15:57:40 -0700

    Added locks to bank account, multithreading

--- AssignmentSeven/src/BankAccount.java
+++ AssignmentSeven/src/BankAccount.java
@@ -1,26 +1,57 @@
 // Create a BankAccount that locks 
-public class BankAccount
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+public class BankAccount 
 {
 	public BankAccount(double originalBalance)
 	{
 		mBalance = originalBalance;
 	}
 	
-    public double getBalance()
+    public synchronized double getBalance()
 	{
 		return mBalance;
 	}
 	
-	public void makeDeposit(double amount)
+	public void makeDeposit(double amount) // would be like producing
 	{
-		mBalance += amount;
+		lock.lock();
+		try {
+			mBalance += amount;
+			depositedMoney.signal();
+		}
+		finally
+		{
+			lock.unlock();
+		}
+		
 	}
 	
-	public void makeWithdrawal(double amount)
+	public void makeWithdrawal(double amount) // would be like consuming
 	{
-		mBalance -= amount;
+		lock.lock();
+		try {
+			
+			while (mBalance - amount < 1)
+			{
+				System.out.println("Waiting to withdraw");
+				depositedMoney.await();
+			}
+			mBalance -= amount;
+		}
+		catch(InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			lock.unlock();
+		}
 	}
 	
 	private double mBalance;
+	private final Lock lock = new ReentrantLock();
+	private final Condition depositedMoney = lock.newCondition();
 		
 }
