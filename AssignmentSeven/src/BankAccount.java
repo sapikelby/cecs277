3315327c29054f6a27f052e1f8ec4319f2022bf0
Kelby Sapien <sapikelby@gmail.com>
2014-04-18 00:17:17 -0700

    Added javadoc documentation

--- AssignmentSeven/src/BankAccount.java
+++ AssignmentSeven/src/BankAccount.java
@@ -1,19 +1,51 @@
-// Create a BankAccount that locks 
+/**
+*
+* CECS 277 Spring 2014.   Assignment 7
+*
+* @author Kelby Sapien 3260
+* @author Paul Kim 6816
+*
+*/
+
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ *	Basic bank account representation with the ability to
+ *	make deposits and withdrawals
+ */
 public class BankAccount 
 {
+	/**
+	 * Custom constructor used to initialize the bank account to a 
+	 * specified initial balance
+	 * @param originalBalance is the account's original balance
+	 */
 	public BankAccount(double originalBalance)
 	{
 		mBalance = originalBalance;
 	}
 	
+	/**
+	 * Returns the bank account's balance
+	 * Synchronized method guarantees that one thread finishes execution, while all
+	 * other threads wait until the first thread is done with the object
+	 * @return the balance of account
+	 */
     public synchronized double getBalance()
 	{
 		return mBalance;
 	}
 	
+    /**
+     * Makes deposit to account 
+     * lock is used to control the threads that want to manipulate mBalance 
+     * lock is unlocked when thread is done with object- other threads have access to lock after unlock
+     * depositedMoney condition is signaled to notify other thread that the condition 
+     * may now be true
+     * @param amount is the amount deposited to the account
+     */
 	public void makeDeposit(double amount) // would be like producing
 	{
 		lock.lock();
@@ -28,6 +60,14 @@ public class BankAccount
 		
 	}
 	
+	/**
+	 * Makes withdrawal to account
+	 * lock is used to control the threads that want to manipulate mBalance
+	 * lock is unlocked when thread is done with object - other threads have access to lock after unlock
+	 * depositedMoney condition makes current thread wait until notified by another thread
+	 * that condition may now be true
+	 * @param amount is the amount withdrawn
+	 */
 	public void makeWithdrawal(double amount) // would be like consuming
 	{
 		lock.lock();
@@ -50,6 +90,12 @@ public class BankAccount
 		}
 	}
 	
+	/**
+	 * 	mBalance is the account's balance
+	 * 	Lock is the lock used to control the manipulation of the account's balance
+	 * 	lock is unlocked when thread is done with object - other threads have access to lock after unlock
+	 * 	depositedMoney is the condition used to notify when a condition may have been been met
+	 */
 	private double mBalance;
 	private final Lock lock = new ReentrantLock();
 	private final Condition depositedMoney = lock.newCondition();
